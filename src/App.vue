80da2c747ff7d668688362284ef111cb09e388c7
Andrey <tinymail8@gmail.com>
2021-03-30 23:34:01 +0300

    init project

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Arvo:wght@700&display=swap");
+#app {
+  font-family: "Arvo", serif;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home-page">
+    <h1 class="home-page__title">100 Days css challenge</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.home-page {
+  &__title {
+    font-size: 38px;
+    text-align: center;
+  }
+}
+</style>
