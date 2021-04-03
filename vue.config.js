7a6abd27844961233344616af22cc2e97997e648
Andrey <tinymail8@gmail.com>
2021-04-04 01:05:14 +0300

    add prerender spa plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require('path');
+const routes = require('./routes');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
 
 module.exports = {
   configureWebpack: {
@@ -7,5 +9,11 @@ module.exports = {
         '@': path.resolve(__dirname, './src'),
       },
     },
+    plugins: [
+      new PrerenderSPAPlugin({
+        staticDir: path.resolve('dist'),
+        routes: [...routes],
+      }),
+    ],
   },
-}
+};
